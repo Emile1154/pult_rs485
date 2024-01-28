317a7b36b20d18fe65e37ad8e4010ed775a849ba
Emile1154 <laim788@gmail.com>
2024-01-29 00:58:51 +0500

    adc config initial

---
+++ src/config/adc_cfg.c
@@ -0,0 +1,13 @@
+#include <config/adc_cfg.h>
+
+static uint16_t adc_filter(){
+
+}
+
+void init_adc(){
+
+}
+
+uint16_t adc_read(uint8_t channel){
+    return 0;
+}
---
+++ src/config/adc_cfg.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef _ADC_CFG_H_
+#define _ADC_CFG_H_
+
+#include <stdint.h>
+
+void     init_adc();
+uint16_t adc_read(uint8_t channel);
+
+#endif
