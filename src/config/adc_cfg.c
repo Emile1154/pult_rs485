41f3a9070a099c40dccea147825f77b4abf59be0
ArturIslamgulov <artur.islamgulov23@mail.ru>
2024-01-29 17:51:23 +0200

    adc config

--- src/config/adc_cfg.c
+++ src/config/adc_cfg.c
@@ -5,9 +5,26 @@ static uint16_t adc_filter(){
 }
 
 void init_adc(){
-
+    ADCSRA |= ( 1 << ADEN );
+    ADCSRA |= ( 1 << ADPS2 )|( 1 << ADPS1 )|( 1 << ADPS0 );
+    ADMUX  |= ( 0 << REFS1 ) | (1 << REFS0 );
+    ADMUX  |= (0 << MUX3 ) | (1 << MUX2 ) | (1 << MUX1 ) | (1 << MUX0);
+    ADMUX  &= ~(1 << ADLAR);
 }
 
+
 uint16_t adc_read(uint8_t channel){
-    return 0;
+    uint32_t mid_ADS = 0;
+    if( channel == 0){
+        ADMUX &= ~(1 << MUX0 );
+    }
+    else{
+        ADMUX |= (1 << MUX0 );
+    }
+    for(uint8_t i = 0 ; i < 100; i++){
+        ADCSRA |= ( 1 << ADSC );
+        while(ADCSRA & (1 << ADSC));
+        mid_ADS =  mid_ADS + ADC;
+    }
+    return (uint16_t)(mid_ADS/100.0);
 }
--- src/config/adc_cfg.h
+++ src/config/adc_cfg.h
@@ -1,7 +1,7 @@
 #pragma once
 #ifndef _ADC_CFG_H_
 #define _ADC_CFG_H_
-
+#include <avr/io.h>
 #include <stdint.h>
 
 void     init_adc();
