c8332b04b17d513b6563c5b1213121c8ba4a5d29
ArturIslamgulov <artur.islamgulov23@mail.ru>
2024-01-30 17:10:22 +0200

    map adc and delete filter

--- src/controller/modbus_controller.h
+++ src/controller/modbus_controller.h
@@ -4,22 +4,23 @@
 
 
 #include <controller/panel_controller.h>
-#include <model/modbus_request.h>
+
 #include <stdint.h>
+#include <model/queue.h>
 
 extern "C" {
     
     #include <form/indicator.h>
 }
 
-#define MAX_REQUESTS 24
+
 
 class ModbusController : public PanelController{
 private:
     uint8_t  state = 0;
     uint64_t timer = 0;
     uint8_t readed = 0;
-    request_modbus_pack_t * requests[MAX_REQUESTS];
+    
     request_modbus_pack_t * request = nullptr;
     
     inline void init_uart(){
@@ -41,6 +42,48 @@ private:
             0x00,
             0x00,
             0x01
+        );
+        requests[2] = new request_modbus_write_pack_t( //START ALL CWW
+            ALL,
+            0x00,
+            0x00,
+            0x00,
+            0x02
+        );
+        requests[3] = new request_modbus_group_write_pack_t( //LAYER START MAX CW
+            LAYER_ID,
+            0x00,
+            0x00,
+            0x00,
+            0x02
+        );
+        requests[4] = new request_modbus_group_write_pack_t( //LAYER_START_MAX_CCW
+            LAYER_ID,
+            0x00,
+            0x00,
+            0x00,
+            0x02
+        );
+        requests[5] = new request_modbus_group_write_pack_t( //LAYER_STOP_SET_SPEED
+            LAYER_ID,
+            0x00,
+            0x00,
+            0x00,
+            0x02
+        );
+        requests[6] = new request_modbus_write_pack_t( //LAYER_SET_SPEED
+            LAYER_ID,
+            0x00,
+            0x01,
+            0x00,
+            0x00
+        );
+        requests[7] = new request_modbus_write_pack_t( //REEL_SET_SPEED
+            REEL_ID,
+            0x00,
+            0x01,
+            0x00,
+            0x00
         );
     }
 
